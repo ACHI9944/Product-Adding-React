d1d7738e98c83104cb282240431e0f9c241ed2ae
ACHI9944 <achi9944@gmail.com>
2022-07-25 22:46:47 +0400

    Product-Adding-React

---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+
+import ExpenseForm from "./ExpenseForm";
+import "./NewExpense.css";
+
+const NewExpense = (props) => {
+  const [isEditing, setIsediting] = useState(false);
+
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString(),
+    };
+    props.onAddExpense(expenseData);
+    setIsediting(false);
+  };
+
+  const startEditingHandler = () => {
+    setIsediting(true);
+  };
+
+  const stopEditinghandler = () => {
+    setIsediting(false);
+  };
+
+  return (
+    <div className="new-expense">
+      {!isEditing && (
+        <button onClick={startEditingHandler}>Add New Expense</button>
+      )}
+      {isEditing && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={stopEditinghandler}
+        />
+      )}
+    </div>
+  );
+};
+
+export default NewExpense;
